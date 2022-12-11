a8996833bd5b9774ad653a15beeb11996c5d3842
Thaneshwara848 <thaneshwaramanjunatha@gmail.com>
2022-12-11 22:04:18 +0530

    prepared statement

--- JdbcDemo2/src/com/Demo2.java
+++ JdbcDemo2/src/com/Demo2.java
@@ -16,4 +16,3 @@ public class Demo2 {
 		}
 	}
 }
-
--- JdbcDemo3/src/com/Demo1.java
+++ JdbcDemo3/src/com/Demo1.java
@@ -1,15 +1,36 @@
 package com;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Demo1 {
-
 	public static void main(String[] args) {
 		try {
 			Class.forName("com.mysql.jdbc.Driver"); // JAR file 
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3307/slbackend","root", "root");
-			Statement stmt=con.createStatement();
-			stmt.execute("insert into employee values(900,'prakash',35,35000,'Dev')");
+//			Statement stmt=con.createStatement();
+//			stmt.execute("insert into employee values(900,'prakash',35,35000,'Dev')");
+			
+			PreparedStatement stmt=con.prepareStatement("insert into employee values(?,?,?,?,?)");
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.print("Enter ID     : ");
+			int id=sc.nextInt();
+			System.out.print("Enter NAME   : ");
+			String name=sc.next();
+			System.out.print("Enter AGE    : ");
+			int age=sc.nextInt();
+			System.out.print("Enter SALARY : ");
+			int salary=sc.nextInt();
+			System.out.print("Enter DESIG  : ");
+			String desig=sc.next();
+			stmt.setInt(1, id);
+			stmt.setString(2,name);
+			stmt.setInt(3, age);
+			stmt.setInt(4, salary);
+			stmt.setString(5, desig);
+			
+			stmt.execute();
 			stmt.close();con.close();
 			System.out.println("Data Inserted Succsfully....!");
 			
@@ -19,3 +40,4 @@ public class Demo1 {
 	}
 
 }
+
